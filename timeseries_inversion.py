51f6fb9d2bbb6354977aa4759f75d967ed4c591f
arimue <ariane.mueting@gmail.com>
2024-08-31 19:13:26 +0200

    added inv with regularization

--- timeseries_inversion.py
+++ timeseries_inversion.py
@@ -13,9 +13,125 @@ from itertools import combinations
 from matplotlib.patches import Arc
 from matplotlib.lines import Line2D
 import scipy.sparse as sp
+import scipy.sparse.linalg
 
 
-# define neccessary functions
+
+def mu_regularisation(regu, A, dates_range):
+    """
+    Compute the Tikhonov regularisation matrix. Code provided by Laurane Charrier.
+
+    :param regu: str, type of regularization
+    :param A: np array, design matrix
+    :param dates_range: list, list of estimated dates
+    :param ini: initial parameter (velocity and/or acceleration mean)
+
+    :return mu: Tikhonov regularisation matrix
+    """
+
+    # First order Tikhonov regularisation
+    if regu == 1:
+        mu = np.identity(A.shape[1], dtype='float32')
+        mu[np.arange(mu.shape[0] - 1) + 1, np.arange(mu.shape[0] - 1)] = -1
+        mu /= (np.diff(dates_range) / np.timedelta64(1, 'D'))
+        mu = np.delete(mu, 0, axis=0)
+
+    # First order Tikhonov regularisation, with an apriori on the acceleration
+    elif regu == '1accelnotnull':
+        mu = np.diag(np.full(A.shape[1], -1, dtype='float32'))
+        mu[np.arange(A.shape[1] - 1), np.arange(A.shape[1] - 1) + 1] = 1
+        mu /= (np.diff(dates_range) / np.timedelta64(1, 'D'))
+        mu = np.delete(mu, -1, axis=0)
+
+    return mu
+def Construction_dates_range_np(data):
+    """
+    Construction of the dates of the estimated displacement in X with an irregular temporal sampling (ILF).
+    Code provided by Laurane Charrier.
+    :param data: np.ndarray, an array where each line is (date1, date2, other elements) for which a velocity have been mesured
+    :return: np.ndarray, the dates of the estimated displacement in X
+    """
+
+    dates = np.concatenate([data[:, 0], data[:, 1]])  # concatante date1 and date2
+    dates = np.unique(dates)  # remove duplicates
+    dates = np.sort(dates)  # Sort the dates
+    return dates
+
+def Construction_A_LF(dates, dates_range):
+    """
+    Construction of the design matix A in the formulation AX = Y. Code provided by Laurane Charrier.
+    
+    :param dates: np array, where each line is (date1, date2) for which a velocity is computed (it corresponds to the original displacements)
+    :param dates_range: list, dates of estimated displacemements in X
+    
+    :return: The design matrix A which represent the temporal closure of the displacement measurement network
+    """
+    # Search at which index in dates_range is stored each date in dates
+    date1_indices = np.searchsorted(dates_range, dates[:, 0])
+    date2_indices = np.searchsorted(dates_range, dates[:, 1]) - 1
+
+    A = np.zeros((dates.shape[0], dates_range[1:].shape[0]), dtype='int32')
+    for y in range(dates.shape[0]):
+        A[y, date1_indices[y]:date2_indices[y] + 1] = 1
+
+    return A
+
+def Inversion_A_LF(A, data, solver, Weight, mu, coef=1, ini=None, result_quality=None,
+                   verbose=False):
+    '''
+    Invert the system AX = Y for one component of the velocity, using a given solver. Code provided by Laurane Charrier.
+
+    :param A: Matrix of the temporal inversion system AX = Y
+    :param data: np array, displacement observation Y
+    :param solver: 'LSMR', 'LSMR_ini', 'LS', 'LS_bounded', 'LSQR'
+    :param Weight: Weight, =1 for Ordinary Least Square
+    :param mu: regularization matrix
+    :param coef: Coef of Tikhonov regularization
+    :param ini: np array, Initialization of the inversion
+    :param: result_quality: None or list of str, which can contain 'Norm_residual' to determine the L2 norm of the residuals from the last inversion, 'X_contribution' to determine the number of Y observations which have contributed to estimate each value in X (it corresponds to A.dot(weight))
+    :param regu : str, type of regularization
+
+    :return X: The ILF temporal inversion of AX = Y using the given solver
+    :return residu_norm: Norm of the residual (when showing the L curve)
+    '''
+
+    v = data
+    D_regu = np.zeros(mu.shape[0])
+    F_regu = np.multiply(coef, mu)
+    if Weight == 1: Weight = np.ones(v.shape[0]) #there is no weight, it corresponds to Ordinary Least Square
+    if solver == 'LSMR':
+        F = np.vstack([np.multiply(Weight[Weight != 0][:, np.newaxis], A[Weight != 0]), F_regu]).astype('float32')
+        D = np.hstack([np.multiply(Weight[Weight != 0], v[Weight != 0]), D_regu]).astype('float32')
+        F = sp.csc_matrix(F)  # column-scaling so that each column have the same euclidian norme (i.e. 1)
+        X = sp.linalg.lsmr(F, D)[0]  # If atol or btol is None, a default value of 1.0e-6 will be used. Ideally, they should be estimates of the relative error in the entries of A and b respectively.
+
+    elif solver == 'LSMR_ini':  # 50ms
+        # 16.7 ms ± 141 µs per loop (mean ± std. dev. of 7 runs, 100 loops each)
+        condi = (Weight != 0)
+        W = Weight[condi]
+        F = sp.csc_matrix(
+            np.vstack([np.multiply(W[:, np.newaxis], A[condi]),
+                       F_regu]))  # stack ax and regu, and remove rows with only 0
+        D = np.hstack([np.multiply(W, v[condi]), D_regu])  # stack ax and regu, and remove rows with only
+
+        if ini.shape[0] == 2:  # if only the average of the entire time series
+            x0 = np.full(len(A.shape [1]) - 1, ini, dtype='float32')
+        else:
+            x0 = ini
+        X = sp.linalg.lsmr(F, D, x0=x0)[0]
+
+    if result_quality is not None and 'Norm_residual' in result_quality:  # to show the L_curve
+        R_lcurve = F.dot(X) - D  # 50.7 µs ± 327 ns per loop (mean ± std. dev. of 7 runs, 10,000 loops each)
+        residu_norm = [np.linalg.norm(R_lcurve[:np.multiply(Weight[Weight != 0], v[Weight != 0]).shape[0]], ord=2),
+                       np.linalg.norm(R_lcurve[np.multiply(Weight[Weight != 0], v[Weight != 0]).shape[0]:] / coef,
+                                      ord=2)]
+    else:
+        residu_norm = None
+
+    return X, residu_norm
+ 
+    
+
 def find_dsoll(row, signal, datecol = 'date', dispcol = 'disp'):
     
     '''Find the displacement that took place between two dates according to the given reference signal.'''
@@ -89,41 +205,67 @@ def solve_matrix_system(A, B, rcond=1e-10, weights = None):
     return ts[:,0]
 
 
-def run_inversion(net, weightcol = None):
+def run_inversion(net, weightcol = None, regu = False):
     
-    '''Run time-series inversion for the given network dataframe'''
+    '''Wrapper for running the time-series inversion for the given network dataframe'''
     
-    net = net.copy()
-    if type(net.date0[0]) == pd._libs.tslibs.timestamps.Timestamp:
-        net.date0 = net.date0.dt.date
-        net.date1 = net.date1.dt.date
-        
-    dates0 = np.asarray([datetime.strptime(str(x), '%Y-%m-%d') for x in net.date0])
-    dates1 = np.asarray([datetime.strptime(str(x), '%Y-%m-%d') for x in net.date1])
-        
     num_pairs = len(net)
-    displacements = np.array(net.disp).reshape((num_pairs, 1))
-
-    # create design_matrix
-    A, date_list = create_design_matrix_cumulative_displacement(num_pairs, dates0, dates1)
-    
     print('Number of image pairs: %d'%num_pairs)
-    nIslands = np.min(A.shape) - np.linalg.matrix_rank(A)
-    print('Number of disconnected components in network: %d '%nIslands)
+        
+    if not regu: #classic inversion 
+        net = net.copy()
+        if type(net.date0[0]) == pd._libs.tslibs.timestamps.Timestamp:
+            net.date0 = net.date0.dt.date
+            net.date1 = net.date1.dt.date
+            
+        dates0 = np.asarray([datetime.strptime(str(x), '%Y-%m-%d') for x in net.date0])
+        dates1 = np.asarray([datetime.strptime(str(x), '%Y-%m-%d') for x in net.date1])
+            
+        displacements = np.array(net.disp).reshape((num_pairs, 1))
+    
+        # create design_matrix
+        A, date_list = create_design_matrix_cumulative_displacement(num_pairs, dates0, dates1)
+    
+        nIslands = np.min(A.shape) - np.linalg.matrix_rank(A)
+        print(f'Number of disconnected groups in network: {nIslands +1}')
+        
+        if weightcol != None:
+            weights = net[weightcol]
+        else:
+            weights = None
+         
+        #run inversion
+        timeseries = solve_matrix_system(A, displacements, rcond=1e-10, weights = weights)
+      
+        out = pd.DataFrame({'date': date_list, 'disp_inversion': timeseries})
+        out['date'] = pd.to_datetime(out.date)
+        
+        return out
 
-    if weightcol != None:
-        weights = net[weightcol]
-    else:
-        weights = None
-     
-    #run inversion
-    timeseries = solve_matrix_system(A, displacements, rcond=1e-10, weights = weights)
-  
-    out = pd.DataFrame({'date': date_list, 'disp_inversion': timeseries})
-    out['date'] = pd.to_datetime(out.date)
-    return out
-
-
+    else: # add a regularization term and solve the inversion
+        
+        data = net[['date0','date1']].values
+        sample_dates = pd.concat([net.date0, net.date1]).unique().astype(np.datetime64)
+        sample_dates.sort()
+    
+        dates_range = Construction_dates_range_np(data)
+        A = Construction_A_LF(data,dates_range)
+        nIslands = np.min(A.shape) - np.linalg.matrix_rank(A)
+        print(f'Number of disconnected groups in network: {nIslands +1}')
+        print("Solving the inversion including a regularization term ...")
+        mu = mu_regularisation(regu=1, A=A, dates_range=sample_dates)
+    
+    
+        timeseries,normresidual = Inversion_A_LF(A, net['disp'].values, solver='LSMR', Weight=1, mu=mu, coef=1, ini=None, result_quality=None,
+                           verbose=False)
+        timeseries_cumulative = np.cumsum(timeseries) #build the cumulative time series bc LF design matrix solves for displacement at each time step
+        timeseries_cumulative = np.insert(timeseries_cumulative, 0, 0, axis=0) #set first date to zero
+        
+        out = pd.DataFrame({'date': sample_dates, 'disp_inversion': timeseries_cumulative})
+        out['date'] = pd.to_datetime(out.date)
+        
+        return out
+        
 
 def plot_results(timeseries, original_signal = None):
     
@@ -302,127 +444,3 @@ def plot_network(network):
     plt.show()
     
     
-###########################################################################################################
-
-# the following code for the inversion of disconnected networks using a regularization term was provided by Laurane Charrier
-
-def mu_regularisation(regu, A, dates_range):
-    """
-    Compute the Tikhonov regularisation matrix
-
-    :param regu: str, type of regularization
-    :param A: np array, design matrix
-    :param dates_range: list, list of estimated dates
-    :param ini: initial parameter (velocity and/or acceleration mean)
-
-    :return mu: Tikhonov regularisation matrix
-    """
-
-    # First order Tikhonov regularisation
-    if regu == 1:
-        mu = np.identity(A.shape[1], dtype='float32')
-        mu[np.arange(mu.shape[0] - 1) + 1, np.arange(mu.shape[0] - 1)] = -1
-        mu /= (np.diff(dates_range) / np.timedelta64(1, 'D'))
-        mu = np.delete(mu, 0, axis=0)
-
-    # First order Tikhonov regularisation, with an apriori on the acceleration
-    elif regu == '1accelnotnull':
-        mu = np.diag(np.full(A.shape[1], -1, dtype='float32'))
-        mu[np.arange(A.shape[1] - 1), np.arange(A.shape[1] - 1) + 1] = 1
-        mu /= (np.diff(dates_range) / np.timedelta64(1, 'D'))
-        mu = np.delete(mu, -1, axis=0)
-
-    return mu
-def Construction_dates_range_np(data):
-    """
-    Construction of the dates of the estimated displacement in X with an irregular temporal sampling (ILF)
-    :param data: np.ndarray, an array where each line is (date1, date2, other elements) for which a velocity have been mesured
-    :return: np.ndarray, the dates of the estimated displacement in X
-    """
-
-    dates = np.concatenate([data[:, 0], data[:, 1]])  # concatante date1 and date2
-    dates = np.unique(dates)  # remove duplicates
-    dates = np.sort(dates)  # Sort the dates
-    return dates
-
-def Construction_A_LF(dates, dates_range):
-    """
-    Construction of the design matix A in the formulation AX = Y
-    
-    :param dates: np array, where each line is (date1, date2) for which a velocity is computed (it corresponds to the original displacements)
-    :param dates_range: list, dates of estimated displacemements in X
-    
-    :return: The design matrix A which represent the temporal closure of the displacement measurement network
-    """
-    # Search at which index in dates_range is stored each date in dates
-    date1_indices = np.searchsorted(dates_range, dates[:, 0])
-    date2_indices = np.searchsorted(dates_range, dates[:, 1]) - 1
-
-    A = np.zeros((dates.shape[0], dates_range[1:].shape[0]), dtype='int32')
-    for y in range(dates.shape[0]):
-        A[y, date1_indices[y]:date2_indices[y] + 1] = 1
-
-    return A
-
-def Inversion_A_LF(A, data, solver, Weight, mu, coef=1, ini=None, result_quality=None,
-                   verbose=False):
-    '''
-    Invert the system AX = Y for one component of the velocity, using a given solver
-
-    :param A: Matrix of the temporal inversion system AX = Y
-    :param data: np array, displacement observation Y
-    :param solver: 'LSMR', 'LSMR_ini', 'LS', 'LS_bounded', 'LSQR'
-    :param Weight: Weight, =1 for Ordinary Least Square
-    :param mu: regularization matrix
-    :param coef: Coef of Tikhonov regularization
-    :param ini: np array, Initialization of the inversion
-    :param: result_quality: None or list of str, which can contain 'Norm_residual' to determine the L2 norm of the residuals from the last inversion, 'X_contribution' to determine the number of Y observations which have contributed to estimate each value in X (it corresponds to A.dot(weight))
-    :param regu : str, type of regularization
-
-    :return X: The ILF temporal inversion of AX = Y using the given solver
-    :return residu_norm: Norm of the residual (when showing the L curve)
-    '''
-
-    if A.shape[0] < A.shape[1]:  # System is under-determined
-        print('The system is under-determined')
-    elif A.shape[0] >= A.shape[1]:  # System is over-determined
-        print('The system is over-determined')
-    if np.linalg.matrix_rank(A) < A.shape[1]:  # System is ill-posed
-        print('The system is ill posed')
-        print('rank A = {}'.format(np.linalg.matrix_rank(A)))
-
-    v = data
-    D_regu = np.zeros(mu.shape[0])
-    F_regu = np.multiply(coef, mu)
-    if Weight == 1: Weight = np.ones(v.shape[0]) #there is no weight, it corresponds to Ordinary Least Square
-    if solver == 'LSMR':
-        F = np.vstack([np.multiply(Weight[Weight != 0][:, np.newaxis], A[Weight != 0]), F_regu]).astype('float32')
-        D = np.hstack([np.multiply(Weight[Weight != 0], v[Weight != 0]), D_regu]).astype('float32')
-        F = sp.csc_matrix(F)  # column-scaling so that each column have the same euclidian norme (i.e. 1)
-        X = sp.linalg.lsmr(F, D)[0]  # If atol or btol is None, a default value of 1.0e-6 will be used. Ideally, they should be estimates of the relative error in the entries of A and b respectively.
-
-    elif solver == 'LSMR_ini':  # 50ms
-        # 16.7 ms ± 141 µs per loop (mean ± std. dev. of 7 runs, 100 loops each)
-        condi = (Weight != 0)
-        W = Weight[condi]
-        F = sp.csc_matrix(
-            np.vstack([np.multiply(W[:, np.newaxis], A[condi]),
-                       F_regu]))  # stack ax and regu, and remove rows with only 0
-        D = np.hstack([np.multiply(W, v[condi]), D_regu])  # stack ax and regu, and remove rows with only
-
-        if ini.shape[0] == 2:  # if only the average of the entire time series
-            x0 = np.full(len(A.shape [1]) - 1, ini, dtype='float32')
-        else:
-            x0 = ini
-        X = sp.linalg.lsmr(F, D, x0=x0)[0]
-
-    if result_quality is not None and 'Norm_residual' in result_quality:  # to show the L_curve
-        R_lcurve = F.dot(X) - D  # 50.7 µs ± 327 ns per loop (mean ± std. dev. of 7 runs, 10,000 loops each)
-        residu_norm = [np.linalg.norm(R_lcurve[:np.multiply(Weight[Weight != 0], v[Weight != 0]).shape[0]], ord=2),
-                       np.linalg.norm(R_lcurve[np.multiply(Weight[Weight != 0], v[Weight != 0]).shape[0]:] / coef,
-                                      ord=2)]
-    else:
-        residu_norm = None
-
-    return X, residu_norm
-  
