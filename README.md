7bdfb89fb7f4ff0f1ebb4b6f3d5e1c22f4457d2d
arimue <ariane.mueting@gmail.com>
2025-12-08 00:13:19 +0100

    added demo and data

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Time-series inversion of a synthetic displacement signal
+# Improved reconstruction of surface displacements through time-series inversion and seasonal bias correction
 
-This repository contains scripts and examples for reconstructing a displacement signal from a network of pairwise displacement measurements through time-series inversion. Inversion is part of the processing chain for deriving a continuous time series from satellite-based measurements of displacements over landslides, glaciers, dunes or other Earth-surface processes. Prerequisite for the inversion are several temporally overlapping displacement fields obtained from image cross-correlation. The inversion process returns a multi-band raster with the cumulative displacement estimated at each unique acquisition time. In addition, this repository provides functionalities to correct for seasonal bias which is common in cross-season image pairs of mountainous terrain. 
+This repository contains scripts and examples for reconstructing a displacement signal from a network of pairwise displacement measurements through time-series inversion. Inversion is part of the processing chain for deriving a continuous time series from satellite-based measurements of displacements over landslides, glaciers, dunes or other Earth-surface processes. Prerequisite for the inversion are several temporally overlapping displacement fields obtained from image cross-correlation. The inversion process returns a multi-band raster with the cumulative displacement estimated at each time step. In addition, this repository provides functionalities to correct for seasonal bias which is common in cross-season image pairs of mountainous terrain and presents a challenge for the identification of seasonally driven displacent.
 
 ## Content
 Please refer to [demo.ipynb](demo.ipynb) for a full walk-through of the time-series inversion process. All core functionality is implemented in [timeseries_inversion.py](./timeseries_inversion.py). 
@@ -14,7 +14,6 @@ To install all necessary Python packages, create a new environment using conda a
 ```
 conda env create -f environment.yml
 conda activate ts_inversion
-jupyter notebook
 ```
 
 
@@ -22,4 +21,4 @@ jupyter notebook
 
 This repository is associated with:
 
-Mueting, A., Charrier, L., and Bookhagen, B.: Reconstructing landslide motion from optical satellite imagery in seasonal climates: insights from the Del Medio catchment, NW Argentina (in prep.)
+Mueting, A., Charrier, L., and Bookhagen, B.: Challenges in reconstructing seasonally driven landslide motion from optical satellite data: insights from the Del Medio catchment, NW Argentina (in prep.)
--- timeseries_inversion.py
+++ timeseries_inversion.py
@@ -6,7 +6,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 from datetime import datetime
-from functools import reduce
 from itertools import combinations
 from matplotlib.patches import Arc
 from matplotlib.lines import Line2D
@@ -14,7 +13,98 @@ import scipy.sparse as sp
 import scipy.sparse.linalg
 import networkx as nx
 import pvlib
+import rasterio
+from tqdm import tqdm
+import os
+from multiprocessing import Pool
 
+
+##########################################################################################################################
+#preprocessing 
+
+def pre_process_for_inversion(files, blocksize = 256, pix_to_m = True, medshift = True, unstable_mask = None, overwrite = False, ensure_same_size = True):
+    
+    files_tiled = []
+    if ensure_same_size: 
+        size_info = []
+        #loop over all files to get their sizes to make sure they all have the same size
+        for file in files:
+            if os.path.isfile(file):
+                with rasterio.open(file) as src: 
+                    meta = src.meta
+                    size_info.append({"filename": file,
+                                      "width": meta["width"],
+                                      "height": meta["height"]})
+                    
+        
+        df = pd.DataFrame(size_info)
+        df = df.sort_values(by=["height", "width"], ascending=[False, False]) #find max size for reference
+        #TODO: select max, pad with nan
+    
+    for file in files: 
+        if not os.path.isfile(file):
+            print(f'File {file} not found')
+            continue
+        outname = f'{file[:-4]}_'
+        if medshift: 
+            outname += 'medshift_'
+        if pix_to_m:
+            outname += 'in_m_'
+        outname += 'tiled.tif'
+            
+        if (not os.path.isfile(outname)) or overwrite: 
+            with rasterio.open(file) as src:
+                profile = src.profile # to be updated for storing the raster later
+                meta = src.meta
+                res = meta['transform'][0]
+                bands = meta['count']
+                
+                if bands > 3: 
+                    print(f'Not sure how to interpret this number of bands. Expected 1-3 bands (3rd band = good pixel mask), got: {bands}.')
+                    continue
+                
+                data = {}
+                
+                for b in range(1, bands+1):
+                    data[f'band_{b}'] = src.read(b)
+                
+            if bands == 3: # assuming input is a 3-band raster from ASP
+                print('Interpreting and applying band 3 as good pixel mask.')
+                
+                data['band_1'][data['band_3'] == 0] = np.nan            
+                data['band_2'][data['band_3'] == 0] = np.nan   
+                del data['band_3']
+            
+            if medshift:
+                if unstable_mask is not None:
+                    with rasterio.open(unstable_mask) as src:
+                        mask = src.read(1)
+                        data['band_1'] -= np.nanmedian(data['band_1'][mask != 1])
+                        data['band_2'] -= np.nanmedian(data['band_2'][mask != 1])
+                        
+                else: 
+
+                        data['band_1'] -= np.nanmedian(data['band_1'])
+                        data['band_2'] -= np.nanmedian(data['band_2'])
+                        
+            if pix_to_m: 
+                
+                for band in data:
+                    data[band] *= res
+                
+            #save file
+            profile.update(dtype='float32', tiled=True, blockxsize=blocksize, blockysize=blocksize, compress='deflate', count = len(data))
+            
+            with rasterio.open(outname, "w", **profile) as dst:
+                for band in range(1, len(data)+1):
+                    dst.write(data[f'band_{band}'], band)     
+                print(f'I have written {outname}.')
+                files_tiled.append(outname)
+    return files_tiled
+        
+
+###########################################################################################################################
+# basic inversion 
 
 def mu_regularisation(regu:str, A:np.array, dates_range:np.array)->np.array:
     """
@@ -61,7 +151,7 @@ def Construction_dates_range_np(data):
 
 def Construction_A_LF(dates, dates_range):
     """
-    Construction of the design matix A in the formulation AX = B. Code provided by Laurane Charrier.
+    Construction of the design matix A in the formulation AX = B using the leap frog approach. Code provided by Laurane Charrier.
     
     :param dates: np array, where each line is (date1, date2) for which a velocity is computed (it corresponds to the original displacements)
     :param dates_range: list, dates of estimated displacemements in X
@@ -77,6 +167,43 @@ def Construction_A_LF(dates, dates_range):
         A[b, date1_indices[b]:date2_indices[b] + 1] = 1
 
     return A
+
+def create_design_matrix_cumulative_displacement(num_pairs, dates0, dates1):
+    '''
+    Create designmatrix connecting the cumulative displacement vector and the displacement measured between each date pair. 
+    Alternative to Construction_A_LF() solving directly for cumulative displacement.
+    Args: 
+        num_pairs: integer value corresponding to the number of pairwise displacement measurements in network. 
+        dates0: array of reference dates. 
+        dates1: array of secondary dates. 
+    Returns: 
+        A: design matrix to be used in the inversion. 
+    '''
+   
+    unique_dates = np.union1d(np.unique(dates0), np.unique(dates1))
+    num_dates = len(unique_dates)
+
+    datepair_list = []
+    for i in range(len(dates0)):
+        datepair_list.append('%s_%s'%(datetime.strftime(dates0[i], '%Y%m%d'), datetime.strftime(dates1[i], '%Y%m%d')))
+
+    A = np.zeros((num_pairs, num_dates), np.float32)
+
+    date_list = list(unique_dates)
+    date_list = [datetime.strftime(d, '%Y%m%d') for d in date_list]
+
+    for i in range(num_pairs):
+        ind1, ind2 = (date_list.index(d) for d in datepair_list[i].split('_'))
+        A[i, ind1] = -1
+        A[i, ind2] = 1
+
+    # Remove reference date as it can not be resolved
+    ref_date = datetime.strftime(min(dates0),'%Y%m%d')
+
+    ind_r = date_list.index(ref_date)
+    A = np.hstack((A[:, 0:ind_r], A[:, (ind_r+1):]))
+
+    return A, date_list
 
 
 def Inversion_A_LF(A, data, solver, Weight, mu, coef=1, ini=None, result_quality=None,
@@ -136,70 +263,9 @@ def Inversion_A_LF(A, data, solver, Weight, mu, coef=1, ini=None, result_quality
     return X, residu_norm
  
     
-
-def find_dsoll(row, signal, datecol = 'date', dispcol = 'disp'):
-    '''
-    Find the displacement that took place between two dates according to the given reference signal.
-    Args: 
-        row: row of a pandas dataframe for which the displacement shall be estimated. 
-        signal: pandas dataframe containing the reference displacement signal based on which pairwise measurements will be estimated. 
-        datecol: string indicating the name of the column in the signal df containing the dates. Default = date
-        dispcol: string indicating the name of the column in the signal df containing the reference displacement. Default = disp. 
-    Returns: 
-        d_soll: displacement value according to provided reference signal. 
-    '''
-    
-    dp0 = signal[signal[datecol] == row.date0]
-    dp1 = signal[signal[datecol] == row.date1]
-    
-    if (len(dp0) > 0) and (len(dp1) > 0):
-        d_soll = (dp1[dispcol].iloc[0] - dp0[dispcol].iloc[0])
-    else: 
-        d_soll =  np.nan
-        
-    return d_soll
-
-
-def create_design_matrix_cumulative_displacement(num_pairs, dates0, dates1):
-    '''
-    Create designmatrix connecting the cumulative displacement vector and the displacement measured between each date pair. 
-    Args: 
-        num_pairs: integer value corresponding to the number of pairwise displacement measurements in network. 
-        dates0: array of reference dates. 
-        dates1: array of secondary dates. 
-    Returns: 
-        A: design matrix to be used in the inversion. 
-    '''
-   
-    unique_dates = np.union1d(np.unique(dates0), np.unique(dates1))
-    num_dates = len(unique_dates)
-
-    datepair_list = []
-    for i in range(len(dates0)):
-        datepair_list.append('%s_%s'%(datetime.strftime(dates0[i], '%Y%m%d'), datetime.strftime(dates1[i], '%Y%m%d')))
-
-    A = np.zeros((num_pairs, num_dates), np.float32)
-
-    date_list = list(unique_dates)
-    date_list = [datetime.strftime(d, '%Y%m%d') for d in date_list]
-
-    for i in range(num_pairs):
-        ind1, ind2 = (date_list.index(d) for d in datepair_list[i].split('_'))
-        A[i, ind1] = -1
-        A[i, ind2] = 1
-
-    # Remove reference date as it can not be resolved
-    ref_date = datetime.strftime(min(dates0),'%Y%m%d')
-
-    ind_r = date_list.index(ref_date)
-    A = np.hstack((A[:, 0:ind_r], A[:, (ind_r+1):]))
-
-    return A, date_list
-
-
 def solve_matrix_system(A, B, weights = None):
     '''
-    Solve matrix system of the form AX = B using least squares without a regularization term. 
+    Solve matrix system of the form AX = B using least squares without a regularization term (only suited for fully connected networks)
     Args: 
         A: design matrix
         B: vector storing pairwise displacement measurements
@@ -299,7 +365,10 @@ def run_inversion(net, dispcol = 'disp', weightcol = None, regu = False):
         out['date'] = pd.to_datetime(out.date)
         
         return out
-        
+
+#############################################################################################################################
+# parallelize inversion to speed up raster processing
+    
 def prep_inversion_parallel(net, verbose = False):
     
     num_pairs = len(net)
@@ -335,6 +404,195 @@ def run_inversion_parallel(disp, A, mu, sample_dates, weights = None):
     out = timeseries_cumulative
     
     return out
+
+def process_window(args):
+    
+    window, A, mu, sample_dates, network, band, weighted = args
+
+    z_stack = []
+    for f in network.filename:
+        with rasterio.open(f) as src:
+            data = src.read(band, window=window)
+            z_stack.append(data)
+    z_stack = np.stack(z_stack, axis=0) 
+
+    h, w = z_stack.shape[1:]
+    result = np.full((h, w, len(sample_dates)), np.nan, dtype=np.float32)
+    weights = None 
+    
+    for i in range(h):
+        for j in range(w):
+            disp_series = z_stack[:, i, j]
+
+            if np.isnan(disp_series).all():
+                result[i, j, :] = np.nan
+            
+            
+            elif np.isnan(disp_series).any():
+                valid_mask = ~np.isnan(disp_series)
+                if valid_mask.sum() < 2:
+                    result[i, j, :] = np.nan
+                    continue
+
+                d_sub = disp_series[valid_mask]
+                net_sub = network[valid_mask]
+                A_sub, mu_sub, sample_dates_sub = prep_inversion_parallel(net_sub)
+                
+                if weighted: 
+                    weights = net_sub["weight"].to_numpy()
+                out = run_inversion_parallel(d_sub, A_sub, mu_sub, sample_dates_sub, weights)
+                
+                if len(sample_dates_sub) != len(sample_dates): #if enough values are missing, maybe not all dates are present and ts is shorter
+
+                    full_series = np.full(len(sample_dates), np.nan, dtype=np.float32)
+                    date_idx = pd.Index(sample_dates).get_indexer(sample_dates_sub)
+
+    
+                    full_series[date_idx] = out
+                    result[i, j, :] = full_series
+                else: 
+                    result[i, j, :] = out
+
+            else: #all data present
+                if weighted: 
+                    weights = network["weight"].to_numpy()
+                    
+                out = run_inversion_parallel(disp_series, A, mu, sample_dates, weights)
+                result[i, j, :] = out
+
+    return result
+
+
+def inversion_tiled(network, band, cpu, ext = "", weights = None):
+        
+    if weights is not None: 
+        network["weight"] = weights
+        weighted = True
+    else: weighted = False
+    
+    network = network.sort_values(["date0", "date1"]).reset_index(drop=True)
+    files = network["filename"].tolist()
+    
+    A, mu, sample_dates = prep_inversion_parallel(network) #precalculate values for when everything is present
+    
+    with rasterio.open(files[0]) as src:
+        if not src.is_tiled:
+            print("Input is not tiled!")
+            return
+        block_height, block_width = src.block_shapes[0] #take first band as reference
+        width, height = src.width, src.height
+        windows = [
+            rasterio.windows.Window(x, y, block_width, block_height)
+            for y in range(0, height, block_height)
+            for x in range(0, width, block_width)
+        ]
+    args = [(window, A, mu, sample_dates, network, band, weighted) for window in windows]
+
+    #run inversion for every tile
+    with Pool(processes=cpu) as pool: 
+            results = list(tqdm(pool.imap(process_window, args), total=len(windows)))
+            
+    #reassemble results
+    full_result = np.full((len(sample_dates), height, width), np.nan, dtype=np.float32)
+    
+    for window, result_block in zip(windows, results):
+        row_off, col_off = int(window.row_off), int(window.col_off)
+        h, w = result_block.shape[:2]
+        full_result[:, row_off:row_off+h, col_off:col_off+w] = result_block.transpose(2, 0, 1)
+    
+    with rasterio.open(files[0]) as src:
+        profile = src.profile
+    
+    profile.update({
+        "count": len(sample_dates),
+        "dtype": "float32",
+        "compress": "deflate",                    
+        "tiled": True
+    })
+    
+    #save
+    if not os.path.isdir('./inversion_out'):
+        os.path.mkdir('./inversion_out')
+        
+    if band == 1: 
+        output_path = f"./inversion_out/inversion{ext}_dx.tif"
+    if band == 2: 
+        output_path = f"./inversion_out/inversion{ext}_dy.tif"
+    
+    with rasterio.open(output_path, "w", **profile) as dst:
+        for i in range(len(sample_dates)):
+            dst.write(full_result[i, :, :], i + 1)
+
+def extract_stats_in_mask(file, mask):
+    
+    with rasterio.open(file) as src: #read all bands into memory
+        inv = src.read()
+
+    with rasterio.open(mask) as src:
+        m = src.read(1)
+            
+    m_selected = m == 1  
+
+    inv_reshaped = inv.reshape(inv.shape[0], -1)  
+
+    selected_pixels = inv_reshaped[:, m_selected.flatten()] 
+
+    stats = pd.DataFrame({"median":np.nanmedian(selected_pixels, axis=1),
+                          "p25":np.nanpercentile(selected_pixels, 0.25, axis=1),
+                          "p75":np.nanpercentile(selected_pixels, 0.75, axis=1), 
+                          "mean":np.nanmean(selected_pixels, axis=1),
+                          "std":np.nanstd(selected_pixels, axis=1)})
+    return stats
+
+
+
+##########################################################################################################################
+# seasonal bias mitigation 
+
+def get_sun_pos(lon, lat, datetime):
+
+    
+    sun_az = pvlib.solarposition.get_solarposition(
+        time=datetime,
+        latitude=lat,
+        longitude=lon
+    )['azimuth'].values[0]
+    
+    sun_el = pvlib.solarposition.get_solarposition(
+        time=datetime,
+        latitude=lat,
+        longitude=lon
+    )['elevation'].values[0]
+    
+    
+    return(sun_az, sun_el)
+
+##########################################################################################################################
+
+# experiments with synthetic networks and displacement signals
+    
+def find_dsoll(row, signal, datecol = 'date', dispcol = 'disp'):
+    '''
+    Find the displacement that should have taken place between two dates according to the given reference signal.
+    Args: 
+        row: row of a pandas dataframe for which the displacement shall be estimated. 
+        signal: pandas dataframe containing the reference displacement signal based on which pairwise measurements will be estimated. 
+        datecol: string indicating the name of the column in the signal df containing the dates. Default = date
+        dispcol: string indicating the name of the column in the signal df containing the reference displacement. Default = disp. 
+    Returns: 
+        d_soll: displacement value according to provided reference signal. 
+    '''
+    
+    dp0 = signal[signal[datecol] == row.date0]
+    dp1 = signal[signal[datecol] == row.date1]
+    
+    if (len(dp0) > 0) and (len(dp1) > 0):
+        d_soll = (dp1[dispcol].iloc[0] - dp0[dispcol].iloc[0])
+    else: 
+        d_soll =  np.nan
+        
+    return d_soll
+
     
 def min_max_scaler(x):
     '''
@@ -417,6 +675,8 @@ def create_random_groups(nr_groups, dates, seed = 123):
     
     return df
 
+##########################################################################################################################
+# visualization
 
 def plot_timeseries(timeseries, original_signal = None, legend = []):
     '''
@@ -514,7 +774,6 @@ def plot_network(network, outname = None):
     
     #get colors for unique groups
     colormap = cm.get_cmap('tab10')
-    #mapping for colors
     colors = {group_id: colormap(i) for i, group_id in enumerate(network.group_id.unique())}
 
     fig, ax = plt.subplots(figsize=(10, 4))
@@ -533,7 +792,6 @@ def plot_network(network, outname = None):
             color= colors[row.group_id]
         ))
         
-    # Create legend lines
     legend = [Line2D([0], [0], color=colors[group_id], lw=1, label=f'Group {group_id+1}') for group_id in network.group_id.unique()]
     ax.legend(handles=legend, loc = 1)
 
@@ -554,21 +812,3 @@ def plot_network(network, outname = None):
         plt.savefig(outname, dpi = 300)
     plt.show()
     
-    
-def get_sun_pos(lon, lat, datetime):
-
-    
-    sun_az = pvlib.solarposition.get_solarposition(
-        time=datetime,
-        latitude=lat,
-        longitude=lon
-    )['azimuth'].values[0]
-    
-    sun_el = pvlib.solarposition.get_solarposition(
-        time=datetime,
-        latitude=lat,
-        longitude=lon
-    )['elevation'].values[0]
-    
-    
-    return(sun_az, sun_el)
